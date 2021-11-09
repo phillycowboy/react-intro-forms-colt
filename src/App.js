9d10bef73f0a2e41343235d489d66403b918dced
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-11-08 20:41:43 -0500

    changes cant use until i update chrome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import Form from './Form';
+import MultipleForm from './MultipleForm';
+// import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      {/* <Form /> */}
+      <MultipleForm/>
     </div>
   );
 }
---
+++ src/MultipleForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+
+class MultipleForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: ""
+        }
+    }
+    handleChange = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        alert(`You Entered: ${this.state.username}`)
+        this.setState({
+            username: ""
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Form Component</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.username} onChange={this.handleChange}/>
+                <button>Submit!</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default MultipleForm
